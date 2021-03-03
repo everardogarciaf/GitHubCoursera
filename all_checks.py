18c696dc1b49d4e107c0d2a8b153c82a0e33c01e
Everardo Garcia Flores <25696114+everardogarciaf@users.noreply.github.com>
2021-03-02 21:34:46 -0800

    Adding current course files to my new respository.

--- all_checks.py
+++ all_checks.py
@@ -3,9 +3,9 @@
 import os 
 import sys
 
-def check_reboot()
+def check_reboot():
     """Returns True if the computer has a pending reboot."""
-    return os.path.exist("/run/reboot-required")
+    return os.path.exists("/run/reboot-required")
 
 def main():
     if check_reboot():
